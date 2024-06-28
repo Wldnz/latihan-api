bfc723929e2201ede31fce72642f163f5f73ae3d
wildanIa <wildanizharalhaqq@gmail.com>
2024-06-28 23:10:48 +0700

    new

--- script.js
+++ script.js
@@ -1,99 +1,110 @@
 
+const containerMovie = document.querySelector('.container');
+const popUpDiv = document.querySelector('.popup');
+let isShowPopup = false;
 
 
-$('.search').on('click',() => {
-    const cariMovie = $('.keyword').val();
-    if (cariMovie === ''){
-        alert('Tolong Inputkan Data Terlebih Dahulu Sebelum Mencari')
-    }else{
-        alert(cariMovie)
-        tampilkanHasilSearch(cariMovie);
+document.body.addEventListener('click',function(e){
+   if (isShowPopup){
+    if(e.target.className === "closePopup"){
+        popUpDiv.style.display = "none";
+        isShowPopup = false;
     }
-    
-
+   }
 })
 
 
-
-// ketika body di klik dan akan mengclose popup
-// function closePop(isShowPop){
-    let isShowPop = false;
-    $('body').on('click',function(e){
-        // $('.popupBox').hide();
-        if (e.target.className === "closePopup"){
-            if(isShowPop){
-                e.target.parentElement.style.display = "none";
-                isShowPop = false;
-            }
-        }
-    }); 
-// }
-// ketika button show Detail di klik
-
-
-
-
-function tampilkanHasilSearch(cariMovie){
-    $.ajax({
-        url : `https://omdbapi.com/?apikey=e1f4312f&s=${cariMovie}`,
-        success : function(res){
-            tampilkanDataKeHtml(res)
-            $('.show-detail').on('click',function (){
-                const getImdbId = $(this).attr('data-detail');
-                $.ajax({
-                    url : `https://omdbapi.com/?apikey=e1f4312f&i=${getImdbId}`,
-                    success : function(res){
-                       let showPop =  `
-                                        <img src="${res.Poster}" alt="this movie doesn't have an poster">
-                                        <div class="desc1">
-                                            <p>Movie Name :${res.Title}</p>
-                                            <p>Release in ${res.Year}</p>
-                                            <p>Writer : ${res.Writer}</p>
-                                            <p>Director : ${res.Director}</p>
-                                            <div class="desc2">
-                                                <p>${res.Plot}</p>
-                                            </div>
-                                        </div>
-                                        <p class="closePopup">X</p>
-                                        `
-                    //    console.log(showPop)
-                       $('.popupBox').html(showPop)
-                       $('.popupBox').show("grid");
-                       isShowPop = true;
-                    }
-                })
-            });
-
-        },
-        error : (err) => {alert('Gagal menampilkan Data')}
+const buttonSearch = document.querySelector('.search')
+    .addEventListener('click',function (){
+        const inputSearch = document.querySelector('.keyword').value;
+        if(inputSearch.length != 0) cariDataSearch(inputSearch); 
     })
+
+
+
+function cariDataSearch(searchValue){
+    fetch('https://omdbapi.com/?apikey=e1f4312f&s=' + searchValue)
+    .then(response => response.json())
+    .then(respon => {
+        if (respon.Response === "False"){
+            return containerMovie.innerHTML = `<h2>${respon.Error}</h2>`; 
+        }
+
+        if(containerMovie.childElementCount > 0){
+            const dataMovieInCard = [...containerMovie.children];
+            dataMovieInCard.forEach(movieCard => movieCard.remove())
+        }
+
+        const movie = respon.Search
+        movie.forEach(element => {
+            containerMovie.innerHTML += showMovieDataIntoHtml(element);
+        });
+
+        tambahkanOnclick(Array.from(document.querySelectorAll('.show-detail')));
+        popUpDiv.style.display = "none";
+        isShowPopup = false;
+    })
+    .catch(err => {
+        return containerMovie.innerHTML = `
+        <h2>Pastikan Kamu Terhubung Dengan Internet</h2>
+        <h2>Please Check Your Internet</h2>
+        <h3>Gagal Mengambil Data</h3>`; 
+    });
+}
+
+function cariImdbData(imdbId){
+    fetch('https://omdbapi.com/?apikey=e1f4312f&i=' + imdbId)
+    .then(response => response.json())
+    .then(respon => {
+        popUpDiv.innerHTML = showPopUpDetail(respon);
+        popUpDiv.style.display = 'grid';
+        isShowPopup = true;
+    })
+    .catch(err => {
+        return containerMovie.innerHTML = `
+        <h2>Pastikan Kamu Terhubung Dengan Internet</h2>
+        <h2>Please Check Your Internet</h2>
+        <h3>Gagal Mengambil Data</h3>`; 
+    });
 }
 
 
-function tampilkanDataKeHtml(data){
-    const movieName = data.Search;
-            // console.log(movieName);
-    
-    let isShowPop = false;
 
-    if (document.querySelector('.container').childElementCount !== 0) {
-        Array.from(document.querySelector('.container').children).forEach(e => e.remove())
-    }
+function showMovieDataIntoHtml(data){
+    return `
+        <div class="item">
+            <img src="${data.Poster}" alt="Gagal menampilkan poster">
+            <h3>${data.Title}</h3>
+            <p>${data.Year}</p>
+            <button class="show-detail" data-imdbid="${data.imdbID}">Show More</button>
+        </div>
+    `;
+}
 
+function tambahkanOnclick(dataArray){
+    showDetailButton = dataArray
+    showDetailButton.forEach(el => {
+        el.addEventListener('click',function(){
+          cariImdbData(this.dataset['imdbid']);
+        })
+    })
+}
 
-    if (movieName === undefined) {
-        return alert('Masukkan Data Film Yang Valid')
-    };
-    let listMovie = movieName.forEach(movie => {
-        let item =  
-        `
-            <div class="item">
-            <img src="${movie.Poster}" alt="this movie doesn't have an poster">
-            <h2 class="title">${movie.Title}</h2>
-            <p class="year">Release in ${movie.Year}</p>
-            <button class= "show-detail" data-detail=${movie.imdbID}>Show More</button>
+function showPopUpDetail(data){
+    return `
+        <h4 class="more-detail">More Detail...</h4>
+        <div class="content">
+            <img src="${data.Poster}" alt="Gagal menampilkan poster">
+            <div class="desc">
+                <h4>${data.Title}7</h4>
+                <p>${data.Released}</p>
+                <p>Director : ${data.Director}</p>
+                <p>Writer : ${data.Writer}</p>
+                <p>${data.Plot}</p>
             </div>
-        `
-        $('.container').append(item); 
-    });
+        </div>
+        <div class="button">
+            <button type="submit" class="closePopup">Close</button>
+        </div>
+    `;
 }
